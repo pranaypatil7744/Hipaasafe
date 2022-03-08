6a89ab03c5d6144d5f3ff9333ecbebabfd1003e2
Pranay <pranay.patil@techstalwarts.com>
2022-03-08 17:29:55 +0530

    Upcoming appointments Scan Qr - UI done

--- app/src/main/java/com/hipaasafe/presentation/home_screen/appointment_fragment/AppointmentFragment.kt
+++ app/src/main/java/com/hipaasafe/presentation/home_screen/appointment_fragment/AppointmentFragment.kt
@@ -1,8 +1,11 @@
 package com.hipaasafe.presentation.home_screen.appointment_fragment
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.*
 import android.view.ViewGroup
 import com.hipaasafe.R
 import com.hipaasafe.base.BaseFragment
@@ -35,12 +38,71 @@ class AppointmentFragment : BaseFragment(), UpcomingAppointmentAdapter.Appointme
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setUpView()
         setUpAdapter()
+        setUpListener()
+    }
+
+    private fun setUpListener() {
+        binding.apply {
+            layoutScanQr.root.setOnClickListener {
+                layoutYourTurn.visibility = VISIBLE
+                setUpWaitingUI(3)
+            }
+            layoutCount.setOnClickListener {
+                setUpWaitingUI(0)
+            }
+            btnGotIt.setOnClickListener {
+                layoutYourTurn.visibility = GONE
+            }
+        }
+    }
+
+    private fun setUpWaitingUI(queueNo: Int) {
+        binding.apply {
+            layoutYourTurn.apply {
+                if (queueNo > 0) {
+                    imgHeart.visibility = INVISIBLE
+                    layoutCount.visibility = VISIBLE
+                    btnGotIt.visibility = GONE
+                    tvCount.text = queueNo.toString()
+                    tvMainTitle.text = context.getString(R.string.we_re_still_waiting_in_the_queue)
+                    tvMainSubTitle.text =
+                        context.getString(R.string.saving_the_world_happens_one_person_at_a_time)
+                } else {
+                    imgHeart.visibility = VISIBLE
+                    layoutCount.visibility = GONE
+                    tvMainTitle.text = getString(R.string.now_it_s_your_turn)
+                    tvMainSubTitle.text =
+                        getString(R.string.a_doctor_sees_pain_death_suffering_on_a_daily_basis_but_they_provide_only_care_and_cure)
+                    btnGotIt.visibility = VISIBLE
+                }
+            }
+        }
+    }
+
+    private fun setUpView() {
+        binding.apply {
+            layoutScanQr.apply {
+                btnAdd.setImageResource(R.drawable.ic_qr_code)
+                tvTitle.text = getString(R.string.clinic_visit)
+                tvSubTitle.text = getString(R.string.make_an_appointment)
+            }
+            layoutYourTurn.apply {
+                tvMainTitle.text = getString(R.string.now_it_s_your_turn)
+                tvMainSubTitle.text =
+                    getString(R.string.a_doctor_sees_pain_death_suffering_on_a_daily_basis_but_they_provide_only_care_and_cure)
+            }
+        }
     }
 
     private fun setUpAdapter() {
         binding.apply {
-            upcomingAppointmentAdapter = UpcomingAppointmentAdapter(requireContext(),upcomingAppointmentList,this@AppointmentFragment)
+            upcomingAppointmentAdapter = UpcomingAppointmentAdapter(
+                requireContext(),
+                upcomingAppointmentList,
+                this@AppointmentFragment
+            )
             recyclerUpcomingAppointment.adapter = upcomingAppointmentAdapter
         }
     }
